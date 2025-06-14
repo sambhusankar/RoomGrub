d8324cb9ece2cb1e5391c88a11c249ce87eeb04b
root <root@sankar.sankar>
2025-06-14 18:09:55 +0530

    login page responsive

--- room-grub/src/app/(auth)/login/page.jsx
+++ room-grub/src/app/(auth)/login/page.jsx
@@ -6,23 +6,32 @@ import LoginBtn from './components/login_btn'
 
 
 export default async function Login(){
-    return(
-        <div className = "h-screen w-screen flex justify-center items-center">
+    return (
+        <div className="min-h-screen min-w-screen flex justify-center items-center bg-gray-50">
             <Box
                 sx={{
-                    height:"120vw",
-                    width:"90vw",
-                    color: 'white',
-                    padding: 2,
-                  }}
-                  className = "flex flex-col justify-center"
+                    width: { xs: '90vw', sm: '400px', md: '400px' },
+                    maxWidth: '95vw',
+                    bgcolor: 'background.paper',
+                    color: 'text.primary',
+                    p: { xs: 2, sm: 4 },
+                    borderRadius: 2,
+                    boxShadow: 3,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
             >
-            <Image src = '/logo.png' 
-                   width = {300}
-                   height = {150}
-                   className = 'w-full'/>
-            <LoginBtn></LoginBtn>
+                <Image
+                    src="/logo.png"
+                    width={300}
+                    height={150}
+                    className="w-full max-w-xs h-auto mb-6"
+                    alt="Logo"
+                    priority
+                />
+                <LoginBtn />
             </Box>
         </div>
-    )
+    );
 }
