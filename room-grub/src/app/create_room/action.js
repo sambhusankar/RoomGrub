1821b907be4b5309889f2e2aec9ab26f0180bfb3
sambhusankar <sambhusankarswain@gmail.com>
2025-07-13 22:34:45 +0530

    fixing create room issue

--- room-grub/src/app/create_room/action.js
+++ room-grub/src/app/create_room/action.js
@@ -2,8 +2,9 @@
 import DB from '@/database';
 import async from 'async';
 import { redirect } from 'next/navigation';
+import { LoginRequired } from '@/policies/LoginRequired';
 const workflow = {
-    insertRoom: async () => {
+    insertRoom: async (session) => {
       // Create a new room and return the created room instance
       const newRoom = await DB.Room.create({
         members: 1,
@@ -11,7 +12,7 @@ const workflow = {
       });
       return newRoom;
     },
-    updateUser: async (roomId) => {
+    updateUser: async (roomId, session) => {
       // Update the user with the new room and role
       await DB.User.update(
         { room: roomId, role: 'Admin' },
@@ -19,25 +20,32 @@ const workflow = {
       );
     }
   };
-  const results = async.auto(workflow);
 
-  const createRoom = async () => {
+export const createRoom = async () => {
     console.log("Creating a room ....");
+    const session = await LoginRequired();
+    console.log(session);
     if (!session) {
       console.error('Auth error: No session');
       return;
     }
-
+    //const results = await async.auto(workflow);
     // Use the workflow to insert the room and update the user in the database
-    const newRoom = results.insertRoom();
+    const newRoom = await DB.Room.create({
+        members: 1,
+        admin: session.user.email
+      });
 
     if (!newRoom || !newRoom.id) {
       console.error("Room creation failed in DB.");
       return;
     }
-
-    results.updateUser(newRoom.id);
+    const roomId = newRoom.id;
+    await DB.User.update(
+        { room: roomId, role: 'Admin' },
+        { where: { email: session.user.email } }
+      );
     console.log("Room inserted successfully:", newRoom);
 
-    redirect(newRoom.id.toString());
-  };
+    redirect(`/${roomId.toString()}`);
+};
