ef52ee8a8ac1b41eea046c1cb61ad5757baa980c
sambhusankar <sambhusankarswain@gmail.com>
2026-04-24 13:07:07 +0530

    auth: moved auth, and member check to layout

--- src/app/[room_id]/layout.js
+++ src/app/[room_id]/layout.js
@@ -1,5 +1,9 @@
 import NavBarContainer from '@/components/NavBarContainer';
+import { LoginRequired } from '@/policies/LoginRequired';
+import { validRoom } from '@/policies/validRoom';
 
-export default function RoomLayout({ children }) {
+export default async function RoomLayout({ children, params }) {
+  await LoginRequired();
+  await validRoom({ params });
   return <NavBarContainer>{children}</NavBarContainer>;
 }
